7397f153b250ec4c2b4ffc802ef6ce711ba2a5c0
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2023-07-10 22:51:09 -0700

    2hw

--- app.py
+++ app.py
@@ -1,36 +1,75 @@
-from flask import Flask
-from datetime import datetime
-import pytz
+"""A simple website with pages for Home, About, List and Contact endpoints."""
+
+from flask import Flask, render_template, jsonify
+import json
+
 
 app = Flask(__name__)
+links = [
+    {"label": "Login", "url": "/login"},
+    {"label": "Home", "url": "/home"},
+    {"label": "About", "url": "/about"},
+    {'label': 'List', 'url': '/list'},
+    {'label': 'Contact', 'url': '/contact'}
+]
 
-@app.route('/')
-def hello_world():
-    return 'Hello World! Welcome to S14A2023 test'
 
-@app.route('/datetime')
-def show_datetime():
-    # Get current datetime in the server's timezone
-    server_time = datetime.now()
+@app.route("/")
+def index():
+    return render_template('index.html', navigation=links)
 
-    # Get current datetime in UTC timezone
-    utc_time = datetime.now(pytz.utc)
 
-    # Get current datetime in your own timezone
-    your_timezone = pytz.timezone('Pacific Standard Time')  # Replace 'Your/Timezone' with your actual timezone
-    your_time = datetime.now(your_timezone)
+@app.route("/home")
+def home():
+    return render_template('home.html', navigation=links)
 
-    # Format the datetime objects as strings
-    server_time_str = server_time.strftime('%Y-%m-%d %H:%M:%S')
-    utc_time_str = utc_time.strftime('%Y-%m-%d %H:%M:%S')
-    your_time_str = your_time.strftime('%Y-%m-%d %H:%M:%S')
+@app.route('/about')
+def about():
+    """A paragraph about the author/site."""
+    return render_template('about.html', title='About', navigation=links)
 
-    # Create the response string
-    response = f"Server Time: {server_time_str}<br>"
-    response += f"UTC Time: {utc_time_str}<br>"
-    response += f"Your Time: {your_time_str}"
+@app.route('/list')
+def list():
+    """
+    Specs:
+        displays a <table> with multiple rows. Data should come
+        from a list of dictionaries from the Flask application. Use Jinja
+        inheritance to construct a base template and the subsequent templates
+        to extend the basetemplate.
 
-    return render_template('index.html', response=response)
+    """
+    # read in json for table Data
+    with open('data.json', 'r') as f:
+        table_data = json.load(f)
+    return render_template('list.html', title='List', navigation=links, table_data=table_data)
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/registration')
+def registration():
+    """
+    Specs:
+        an HTML form with fields for first name, last name, e-mail,
+        password and a button. The form should be submitted to the same
+        URL using the POST method. The form should be validated using
+        Flask-WTF. If the form is valid, the user should be redirected
+        to the /l
+    """
+    return render_template('registration.html', title='Registration', navigation=links)
+
+@app.route('/login')
+def login():
+    """
+    Specs:
+        an HTML form with fields for e-mail, password and a button.
+        The form should be submitted to the same URL using the POST method.
+        The form should be validated using Flask-WTF. If the form is valid,
+        the user should be redirected to the /list endpoint.
+    """
+    return render_template('login.html', title='Login', navigation=links)
+
+@app.route('/contact')
+def contact():
+#    Contact is an HTML form, where people 
+#    need to put in their e-mail and a message in a <textarea> textbox, with a button.
+    return render_template('contact.html', title='contact', navigation=links)
+
+
